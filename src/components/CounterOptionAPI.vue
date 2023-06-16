d35396a1db91370447191d7f42ae78a7bc0b4fae
DavidDevVelandia <davidvelandiadeveloper@gmail.com>
2023-06-15 19:18:01 -0500

    CounterOptionAPI has finished

---
+++ src/components/CounterOptionAPI.vue
@@ -0,0 +1,20 @@
+<template>
+    <h3></h3>
+    <button>Increase</button>
+</template>
+
+<script>
+    import { defineComponent } from 'vue';
+    export default defineComponent({
+        name: `CounterOptionAPI`,
+        data() {
+            let counter = 0;
+            return {counter}
+        },
+        methods: {
+            addCounter(){
+                this.counter++
+            }
+        }
+    })
+</script>
