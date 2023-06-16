35169720431b2659776536c33c9a15c5e9d571f5
DavidDevVelandia <davidvelandiadeveloper@gmail.com>
2023-06-15 19:25:33 -0500

    Counter applied in App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,10 @@
 <template>
   <PostContainer/>
+  <CounterOptionAPI/>
 </template>
 <script setup>
   import PostContainer from './components/PostContainer.vue';
+  import CounterOptionAPI from './components/CounterOptionAPI.vue';
 </script>
 <style>
 
