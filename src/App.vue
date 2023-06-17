ced181eb2cf4817fdd9e1924c42c984c086bb219
DavidDevVelandia <davidvelandiadeveloper@gmail.com>
2023-06-17 18:28:50 -0500

    Counter with Composition API has been include

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,16 @@
 <template>
   <PostContainer/>
+
+  <h1>CounterOption</h1>
   <CounterOptionAPI/>
+  
+  <h1>CompositionAPI</h1>
+  <CounterCompositionAPI/>
 </template>
 <script setup>
   import PostContainer from './components/PostContainer.vue';
   import CounterOptionAPI from './components/CounterOptionAPI.vue';
+  import CounterCompositionAPI from './components/CounterCompositionAPI.vue';
 </script>
 <style>
 
---
+++ src/components/CounterCompositionAPI.vue
@@ -0,0 +1,14 @@
+<template>
+    <h3>{{ counter }}</h3>
+    <button @click="addCounter">Incrementar</button>
+</template>
+<script setup>
+import { ref } from "vue"
+    //let counter = 0; se declara la variable pero no es reactiva
+
+    //con ref() indica que la variable sera reactiva
+    let counter = ref(0);
+    const addCounter = () => {
+        counter.value++
+    }
+</script>
